08c12d495d2ca9f530189bb9841797d7796b6203
José Albuquerque <silvanoalbuquerque@gmail.com>
2022-02-20 22:16:20 -0300

    working with text style

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -14,27 +15,26 @@ import com.example.composecourseyt.components.ImageCard
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        showImageCards()
+        setContent {
+        }
     }
 
-    private fun showImageCards() {
-        setContent {
-            val painter = painterResource(id = R.drawable.kemit)
-            val description = "Kermit is resting"
-            val title = "Kermit is resting and thing about his life"
+    @Composable
+    private fun ShowImageCards() {
+        val painter = painterResource(id = R.drawable.kemit)
+        val description = "Kermit is resting"
+        val title = "Kermit is resting and thinking about his life"
 
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .padding(16.dp)
-            ) {
-                ImageCard(
-                    painter = painter,
-                    contentDescription = description,
-                    title = title
-                )
-            }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(16.dp)
+        ) {
+            ImageCard(
+                painter = painter,
+                contentDescription = description,
+                title = title
+            )
         }
     }
 }
